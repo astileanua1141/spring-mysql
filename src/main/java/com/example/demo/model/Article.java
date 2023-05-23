15532d3f221b5ca3a10335027675aa5ddf6ee453
astileanua1141 <alexandra.astileanu@gmail.com>
2023-05-23 12:55:13 +0300

    First commit

---
+++ src/main/java/com/example/demo/model/Article.java
@@ -0,0 +1,41 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+    private String name;
+    private String category;
+
+    public Article() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
